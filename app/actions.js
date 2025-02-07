53850c08e1f2a87999178df984e29ef32ba9db1b
Krista Goralczyk <krista.goralczyk@gmail.com>
2025-02-07 16:56:32 -0500

    Render the recommended products

--- app/actions.js
+++ app/actions.js
@@ -17,38 +17,35 @@ export async function getProduct(id) {
 
 
 // Server-side AI example
-import PipelineSingleton from '@/app/pipeline.js';
+// import PipelineSingleton from '@/app/pipeline.js';
 
-export async function getAiRecommendations(userTraits = [], category) {
-  if (!Array.isArray(userTraits) || !userTraits.length) {
-		// If no user trait data, query 3 'default' products in similar category
-		// saves an AI API call and prevents needless server resource usage
-		return await listProducts({
-			conditions: [
-				{ attribute: 'category', value: category, comparator: 'equals' },
-			],
-			limit: 3
-		});
-  }
+// export async function getAi_OLD(userTraits = [], category) {
+//   if (!Array.isArray(userTraits) || !userTraits.length) {
+// 		// If no user trait data, query 3 'default' products in similar category
+// 		// saves an AI API call and prevents needless server resource usage
+// 		return await listProducts({
+// 			conditions: [
+// 				{ attribute: 'category', value: category, comparator: 'equals' },
+// 			],
+// 			limit: 3
+// 		});
+//   }
 
-  // Get the question-answer pipeline. When called for the first time,
-  // this will load the pipeline and cache it for future use.
-  const answerer = await PipelineSingleton.getInstance();
+//   // Get the question-answer pipeline. When called for the first time,
+//   // this will load the pipeline and cache it for future use.
+//   const answerer = await PipelineSingleton.getInstance();
 
-  // Actually perform the product recommendation
-	return await listProducts()
-		.then(data => {
-			const question =
-				`What are three good product IDs for someone who has the following traits: ${userTraits.join(', ')}`;
-			const context = `Good products for the ${userTraits.join(', ')} can be pulled from this list of product data: ${data}, and good products have similar names, descriptions, and features to the ${userTraits} please return the product ids of the selected products.`;
+//   // Actually perform the product recommendation
+// 	return await listProducts()
+// 		.then(data => {
+// 			const question =
+// 				`What are three good product IDs for someone who has the following traits: ${userTraits.join(', ')}`;
+// 			const context = `Good products for the ${userTraits.join(', ')} can be pulled from this list of product data: ${data}, and good products have similar names, descriptions, and features to the ${userTraits} please return the product ids of the selected products.`;
 
-			return answerer(question, context)
-				// .then(res => {
-				// 	console.log('okay... ', res.answer);
-				// 	return res.answer;
-				// });
-		});
-}
+// 			return answerer(question, context)
+// 				.then(res => res.answer);
+// 		});
+// }
 
 // -------
 import OpenAI from "openai";
@@ -58,7 +55,7 @@ const openai = new OpenAI({
 	project: process.env.OPENAI_PROJECT_ID,
 });
 
-export async function getAi(userTraits = [], category) {
+export async function getAiRecommendations(userTraits = [], category) {
   if (!Array.isArray(userTraits) || !userTraits.length) {
 		// If no user trait data, query 3 'default' products in similar category
 		// saves an AI API call and prevents needless server resource usage
