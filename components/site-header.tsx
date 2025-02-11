bf27a9be10960002b87d8a014a377aee3abef70e
Krista Goralczyk <krista.goralczyk@gmail.com>
2025-02-11 12:08:09 -0500

    adding algolia search

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,10 +1,16 @@
 'use client';
 
-import { ShoppingBag } from "lucide-react";
+import { ShoppingBag, Search } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuPortal, DropdownMenuContent } from "./ui/dropdown-menu";
+import { Input } from "./ui/input";
 
 export function SiteHeader() {
+  function search(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log('Algolia will search: ', e.target.value);
+  }
+  
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
@@ -16,6 +22,29 @@ export function SiteHeader() {
           <Link href="/products" className="text-sm font-medium hover:text-primary">
             Products
           </Link>
+
+          <DropdownMenu>
+
+            <DropdownMenuTrigger asChild>
+              <Button size="icon" variant="ghost" onClick={() => {}}>
+                <Search className="h-5 w-5" />
+              </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuPortal>
+              <DropdownMenuContent style={{ width: 400, padding: 10 }}>
+                <h3 style={{ paddingBottom: 5 }}>Search</h3>
+                <div>
+                  <Input type="text" onChange={search} />
+                </div>
+                <div style={{ paddingTop: 10, paddingBottom: 10 }}>
+                  TODO: Algolia search results here
+                </div>
+              </DropdownMenuContent>
+            </DropdownMenuPortal>
+          </DropdownMenu>
+          
+          
           <Button size="icon" variant="ghost">
             <ShoppingBag className="h-5 w-5" />
           </Button>
